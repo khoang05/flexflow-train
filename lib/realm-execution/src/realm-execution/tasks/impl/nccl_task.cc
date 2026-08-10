442a52b0fe14a562c781b2c2a2be48ac39698adb
Kim Hoang <kimhoang1229@gmail.com>
2026-08-10 13:26:35 -0700

    Wire NCCL task body to task arguments

--- lib/realm-execution/src/realm-execution/tasks/impl/nccl_task.cc
+++ lib/realm-execution/src/realm-execution/tasks/impl/nccl_task.cc
@@ -1,8 +1,17 @@
+#include "op-attrs/ops/replicate_attrs.dtg.h"
+#include "op-attrs/pcg_operator_attrs.dtg.h"
+#include "realm-execution/device_specific_managed_per_device_ff_handle.h"
+#include "realm-execution/dynamic_tensor_accessor_from_instance.h"
 #include "realm-execution/tasks/impl/nccl_task.h"
 #include "realm-execution/tasks/impl/nccl_task_args.dtg.h"
 #include "realm-execution/tasks/impl/serializable_nccl_task_args.h"
 #include "realm-execution/tasks/serializer/task_arg_serializer.h"
 #include "realm-execution/tasks/task_id_t.h"
+#include "task-spec/dynamic_graph/dynamic_value_attrs.dtg.h"
+#include "task-spec/dynamic_graph/training_operation_attrs.dtg.h"
+#include "task-spec/permissions.h"
+#include "utils/containers/map_values.h"
+#include "utils/optional.h"
 
 #include <cstdio>
 #include <nccl.h>
@@ -10,12 +19,12 @@
 namespace FlexFlow {
 
 ncclResult_t run_nccl_all_reduce(void const *send_buffer,
-                                void *receive_buffer,
-                                size_t count,
-                                ncclDataType_t data_type,
-                                ncclRedOp_t reduction_op,
-                                ncclComm_t communicator,
-                                ffStream_t stream) {
+                                 void *receive_buffer,
+                                 size_t count,
+                                 ncclDataType_t data_type,
+                                 ncclRedOp_t reduction_op,
+                                 ncclComm_t communicator,
+                                 ffStream_t stream) {
   return ncclAllReduce(send_buffer,
                        receive_buffer,
                        count,
@@ -66,10 +75,52 @@ void nccl_task_body(void const *args,
                     Realm::Processor proc) {
   (void)userdata;
   (void)userdata_len;
-  (void)proc;
 
-  NCCLTaskArgs task_args = nccl_task_args_from_serializable(
-      deserialize_task_args<SerializableNcclTaskArgs>(args, arglen));
+NCCLTaskArgs task_args = nccl_task_args_from_serializable(
+    deserialize_task_args<SerializableNcclTaskArgs>(args, arglen));
+
+  RealmContext ctx{proc};
+
+  device_handle_t device_handle =
+      device_handle_t_from_device_specific_managed_ff_handle(
+          task_args.device_handle,
+          ctx.get_current_global_device_id());
+
+  auto map_instance_to_accessor = [&](DynamicValueAttrs const &value) {
+    DynamicValueAttrs result = value;
+
+    auto const &[inst, event] =
+        task_args.tensor_backing.backing.at(value);
+
+    result.accessor = dynamic_tensor_accessor_from_instance(
+        inst,
+        event,
+        assert_unwrap(value.parallel_tensor_shape),
+        Permissions::RW,
+        ctx.get_current_processor());
+
+    return result;
+  };
+
+  DynamicNodeInvocation invocation = task_args.invocation;
+
+  invocation.inputs =
+      map_values(invocation.inputs, map_instance_to_accessor);
+
+  invocation.outputs =
+      map_values(invocation.outputs, map_instance_to_accessor);
+
+  TrainingOperationAttrs const &training_op_attrs =
+      assert_unwrap(invocation.node_attrs.op_attrs);
+
+  PCGOperatorAttrs const &pcg_op_attrs =
+      training_op_attrs.require_pcg_op();
+
+  ReplicateAttrs const &replicate_attrs =
+      pcg_op_attrs.require_parallel_replicate();
+
+  (void)device_handle;
+  (void)replicate_attrs;
 
   int nccl_version = 0;
   ncclResult_t result = ncclGetVersion(&nccl_version);
